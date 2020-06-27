bf8b1e6baaa4756003d917658482a08b32d8db33
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-27 16:37:46 +0100

    Finish exercise 04_04

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,21 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        int intValue = 42;
+        var fromInt = Integer.toString(intValue);
+        System.out.println(fromInt);
+
+        boolean boolValue = true;
+        var fromBool = Boolean.toString(boolValue);
+        System.out.println(fromBool);
+
+        // underscores for readability
+        long longValue = 10_000_000;
+        var fromLong = Long.toString(longValue);
+        System.out.println(fromLong);
     }
 }
