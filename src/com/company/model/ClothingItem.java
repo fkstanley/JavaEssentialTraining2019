e3652a9b75d7693b44249e8545a4932193998a54
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-27 21:05:20 +0100

    Finish exercise 07_03

---
+++ src/com/company/model/ClothingItem.java
@@ -0,0 +1,40 @@
+package com.company.model;
+
+public class ClothingItem {
+    private String type;
+    private String size;
+    private double price;
+    private int quantity;
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
