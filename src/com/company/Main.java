89a4a2b0bf7f739850a17be77695491f2d9d01fc
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-27 17:11:15 +0100

    Finish challenge 04_10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner input = new Scanner(System.in);
+
+    	System.out.print("Enter a numeric value: ");
+	    var d1 = input.nextDouble();
+	    System.out.print("Enter a numeric value: ");
+	    var d2 = input.nextDouble();
+
+	    System.out.println("The answer is: " + d1 / d2);
     }
 }
