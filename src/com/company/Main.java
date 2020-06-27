7856bada03cbc78138a155d1fca4e33a73968b9b
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-27 19:23:31 +0100

    Finish exercise 05_05

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,12 @@ public class Main {
 
         double result = addValues(i1, i2);
         System.out.println("The sum is " + result);
+
+        double resultOf4 = addValues(i1, i2, i1, i2);
+        System.out.println("The sum is " + resultOf4);
+
+        double resultOfMultiple = addValues(i1, i2, i1, i2, i1, i2);
+        System.out.println("The sum is " + resultOfMultiple);
     }
 
     private static int getInput(Scanner sc, String prompt) {
@@ -20,6 +26,17 @@ public class Main {
 
     private static double addValues(int i1, int i2) {
         return i1 + i2;
+    }
+    private static double addValues(int... values) {
+        int result = 0;
+        for (var value : values) {
+            result += value;
+        }
+        return  result;
+    }
+
+    private static double addValues(int i1, int i2, int i3, int i4) {
+        return i1 + i2 + i3 + i4;
     }
 
 }
