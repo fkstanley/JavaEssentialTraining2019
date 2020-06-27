2e3f20f56839dbdb5d8c7241bd6130a0f8f1d517
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-27 16:57:31 +0100

    Finish exercise 04_07

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
         String s1 = "Hello!";
+        // s2 is created as a reference to s1 automatically by Java
         var s2 = "Hello!";
+
+        if (s1 == s2) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match...");
+        }
+
+        String s3 = new String("Hello!");
+        String s4 = new String("Hello!");
+
+        // Wrong way
+        if (s3 == s4) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match...");
+        }
+
+        // Correct way
+        if (s3.equals(s4)) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match...");
+        }
+
+        var s5 = "HELLO!";
+
+        if (s3.equalsIgnoreCase(s5)) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match...");
+        }
     }
 }
